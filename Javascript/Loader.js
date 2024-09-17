1c40128f23efb3a3db29f6c431ebc02ea701e5e0
MiesieduoCodes <miesieduoveria@gmail.com>
2024-09-17 21:52:17 +0100

    started

--- Javascript/Loader.js
+++ Javascript/Loader.js
@@ -1,7 +1,12 @@
         // Set loading duration (in milliseconds)
         const loadingDuration = 3000; // 3 seconds
 
-        setTimeout(() => {
-            const loadingText = document.getElementById('loadingText');
-            loadingText.classList.add('zoom');
-        }, loadingDuration);
+        window.onload = function() {
+            setTimeout(() => {
+                const loadingText = document.getElementById('loadingText');
+                loadingText.classList.add('zoom');
+                setTimeout(() => {
+                    // Redirect to another page
+                    window.location.href = '/HTML/Home.html'; // Replace with your target page
+                }, 1000); // Wait for the zoom animation to finish
+            }, loadingDuration)};
